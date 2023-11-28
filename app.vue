fc3dff35d99462f24b7e238cf2a7bd26885a74fa
Akhbaralvian <alvian5@gmail.com>
2023-11-28 14:13:29 +0700

    up-config-footer

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-cover bg-center opacity-85" style="background-image: url(/img/moon.jpg);">
+  <div class="bg-cover bg-center opacity-85 min-h-screen" style="background-image: url(/img/moon.jpg);">
     <NuxtPage />
   </div>
 </template>
